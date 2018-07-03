5d1b76c09bee724fa1f143e8a272a3ea8e0f968d
Abhishek <abhi.info.dev@gmail.com>
2018-07-03 11:21:27 +0530

    Added supports for old browsers

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -2,25 +2,31 @@
 
 function sanitizeFormUsername($inputText) {
     $inputText = strip_tags($inputText);
+    // strip_tags remove all the html elements that might be on string.
     $inputText = str_replace(" ", "", $inputText);
+    // replace all the spaces with empty string.
     return $inputText;
 }
 
 function sanitizeFormString($inputText) {
     $inputText = strip_tags($inputText);
+    // strip_tags remove all the html elements that might be on string.
     $inputText = str_replace(" ", "", $inputText);
+    // replace all the spaces with empty string.
     $inputText = ucfirst(strtolower($inputText));
+    // ucfirst - will uppercase first character. strtolower will lower case to all characters.
     return $inputText;
 }
 
 function sanitizeFormPassword($inputText) {
     $inputText = strip_tags($inputText);
+    // strip_tags remove all the html elements that might be on string.
     return $inputText;
 }
 
 if(isset($_POST['registerButton'])) {
     //Register button was pressed
-    $username = sanitizeFormUsername($_POST['userName']);
+    $userName = sanitizeFormUsername($_POST['userName']);
     $firstName = sanitizeFormString($_POST['firstName']);
     $lastName = sanitizeFormString($_POST['lastName']);
     $email = sanitizeFormString($_POST['email']);
@@ -28,7 +34,7 @@ if(isset($_POST['registerButton'])) {
     $password = sanitizeFormPassword($_POST['password']);
     $password2 = sanitizeFormPassword($_POST['password2']);
 
-    $wasSuccessful = $account->register($username, $firstName, $lastName, $email, $email2, $password, $password2);
+    $wasSuccessful = $account->register($userName, $firstName, $lastName, $email, $email2, $password, $password2);
 
     if($wasSuccessful) {
         $_SESSION['userLoggedIn'] = $userName;
