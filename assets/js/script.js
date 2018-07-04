26505c11fc382b4fc25c3a7e58944e9f3991c56d
Abhishek <abhi.info.dev@gmail.com>
2018-07-04 23:43:39 +0530

    Auto next song functionality after song end.

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,10 @@ function Music() {
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
 
+    this.audio.addEventListener("ended", function() {
+       nextSong();
+    });
+
     this.audio.addEventListener("canplay", function() {
         let duration = formatTime(this.duration);
         $(".progressTime.remaining").text(duration);
